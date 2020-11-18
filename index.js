3c4aa1f5bc1a9f0a7d035157e33cabdd6aaeae23
jordanrob709 <jordanrob709@gmail.com>
2020-11-18 20:00:54 +0300

    Exercise 3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -46,6 +46,18 @@ app.get('/info', (request, response) => {
     )
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if(!person){
+        response.status(404).end()
+    }else{
+        response.json(person)
+    }
+
+})
+
 
 PORT = 3001
 app.listen(PORT, () => {
